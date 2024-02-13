cb3007dce62298a2ec2c82b6e8620f96079bbcbf
ajegorovs <65294626+ajegorovs@users.noreply.github.com>
2024-02-13 13:59:03 +0200

    upgraded print_latex output for string arrays, edited relateds stuff on lin alg transforms file.

--- misc_tools/print_latex.py
+++ misc_tools/print_latex.py
@@ -1,18 +1,33 @@
-
 import numpy as np
 from IPython.display import display, Math
 from latexifier import latexify
+import warnings
 
-def print_tex(*args,column = True, style_fraction = 'inline'):
+def string_matrix(matrix):
+    """Format a matrix of strings using LaTeX syntax"""
+    body_rows = [' & '.join(map(str,row)) for row in matrix]
+    body_rows_join  =    r' \\ '.join(body_rows)
+    return r"\begin{bmatrix}" + body_rows_join + r"\end{bmatrix}"
+
+def print_tex(*args,column = False, style_fraction = 'inline'):
+
     a = ''
     for arg in args:
         if type(arg) != str:
-            arg = latexify(arg, newline=False, arraytype="bmatrix", column = column, style_fraction = style_fraction)
-        else:
-            arg = r'\text{' + arg + '} '
+            if arg.dtype.type is not np.str_:
+                with warnings.catch_warnings():
+                    warnings.filterwarnings("ignore", category=UserWarning)
+                    arg = latexify(arg, newline=False, arraytype="bmatrix", column = column, style_fraction = style_fraction)
+            else:
+                arg = string_matrix(arg)
+        # else:
+        #     arg = r'\text{' + arg + '} '
         a += arg
     display(Math(a))
 
-
-print_tex('(Latex print example) I = : ', np.diag(range(3)), column=False)
-
+print("input example : ")
+print(r">>> arr_T = np.array([[r'\vec{v}_1', r'\vec{v}_2']]).T")
+print(r">>> print_tex(arr_T,'=', np.arange(1,5).reshape(2,-1)/4, r'; symbols: \otimes, \cdot,\times')")
+print("output: ")
+arr_T = np.array([[r'\vec{v}_1', r'\vec{v}_2']]).T
+print_tex(arr_T,'=', np.arange(1,5).reshape(2,-1)/4, r'; symbols: \otimes, \cdot,\times')
